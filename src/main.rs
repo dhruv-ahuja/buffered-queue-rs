935cc753c488b157b0166a752b7c10921e7371be
Dhruv Ahuja <dhruvahuja2k@gmail.com>
2023-07-19 23:08:05 +0530

    feat: implement queue, consumer and producer threads
    - use atomic bools for signal passing between threads
    - create consumer and producer threads as pipelines

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,71 @@
 pub mod queue;
 
+use queue::BufferedQueue;
+use std::{
+    sync::{atomic::Ordering, Arc, Mutex},
+    thread::{self, sleep},
+    time::Duration,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let order = Ordering::Relaxed;
+
+    let queue: Arc<BufferedQueue<i32>> = Arc::new(BufferedQueue::new(3));
+    let consumer_queue = queue.clone();
+
+    let output = Arc::new(Mutex::new(Vec::new()));
+
+    let producer_handle = thread::spawn(move || {
+        println!("initializing producer thread...\n");
+
+        let mut nums = 1..=10;
+
+        loop {
+            if queue.is_full_.load(order) {
+                sleep(Duration::from_millis(75));
+                continue;
+            }
+
+            let num;
+            if let Some(val) = nums.next() {
+                num = val;
+            } else {
+                println!("\nexhausted range, terminating producer!\n");
+                queue.elements_processed.store(true, order);
+                return;
+            };
+
+            // mock processing of the input data
+            let processed_num = num * num * num;
+            sleep(Duration::from_millis(250));
+
+            queue.push(processed_num);
+            println!("pushed element {}", processed_num);
+        }
+    });
+
+    let consumer_handle = thread::spawn(move || {
+        println!("initializing consumer thread...\n");
+
+        loop {
+            if consumer_queue.is_empty_.load(order) {
+                if consumer_queue.elements_processed.load(order) {
+                    println!("exhausted queue, terminating consumer!\n");
+                    return;
+                }
+                sleep(Duration::from_millis(150));
+                continue;
+            }
+
+            let mut output_vec = output.lock().unwrap();
+
+            // we can safely unwrap as we know there's an element in the queue
+            let num = consumer_queue.pop().unwrap();
+            output_vec.push(num);
+            println!("pushed to output num: {}", num);
+        }
+    });
+
+    consumer_handle.join().unwrap();
+    producer_handle.join().unwrap();
 }
--- src/queue.rs
+++ src/queue.rs
@@ -1,5 +1,12 @@
 use std::collections::VecDeque;
-use std::sync::{Condvar, Mutex, MutexGuard};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Mutex, MutexGuard};
+
+/// Operation represents the performed operation on the queue -- push or pop
+enum Operation {
+    Push,
+    Pop,
+}
 
 /// BufferedQueue is a queue implementation with a pre-defined maximum capacity, for workloads where one part of the
 ///  pipeline is faster than other parts and processes tasks much faster than the other parts' consumption ability.
@@ -9,49 +16,37 @@ use std::sync::{Condvar, Mutex, MutexGuard};
 /// consumption.
 pub struct BufferedQueue<T> {
     /// represents the internal queue implementation, wrapped in a mutex
-    data: Mutex<VecDeque<T>>,
+    pub data: Mutex<VecDeque<T>>,
 
     /// represents the maximum number of elements allowed in the queue at a given time
     capacity: usize,
 
+    /// signals to the producer thread that the queue is full
+    pub is_full_: AtomicBool,
+
     /// signals to the consumer thread that the queue is empty
-    is_empty_signal: Condvar,
+    pub is_empty_: AtomicBool,
 
-    /// boolean flag to signify to all threads whether the queue is empty
-    is_empty: Mutex<bool>,
-
-    /// signals to producer threads that the queue is at the defined capacity
-    is_full_signal: Condvar,
-
-    /// boolean flag to signiy to all threads whether the queue is full
-    is_full: Mutex<bool>,
+    /// signals termination for the consumer thread
+    pub elements_processed: AtomicBool,
 }
 
 impl<T> BufferedQueue<T> {
     pub fn new(capacity: usize) -> Self {
-        let is_nonempty_signal = Condvar::new();
-        let is_empty = Mutex::new(true);
-
-        let is_full_signal = Condvar::new();
-        let is_full = Mutex::new(false);
-
         Self {
             data: Mutex::new(VecDeque::with_capacity(capacity)),
             capacity,
-            is_empty_signal: is_nonempty_signal,
-            is_empty,
-            is_full_signal,
-            is_full,
+            is_full_: AtomicBool::new(false),
+            is_empty_: AtomicBool::new(true),
+            elements_processed: AtomicBool::new(false),
         }
     }
 
     /// ensures that the calling function acquires the mutex guard only if the queue has space
     fn ensure_has_space(&self) -> Option<MutexGuard<'_, VecDeque<T>>> {
         let queue = self.data.lock().unwrap();
-        if queue.len() == self.capacity {
-            return None;
-        }
-        Some(queue)
+        println!("queue has space");
+        (queue.len() != self.capacity).then_some(queue)
     }
 
     /// pushes an element to the back of the queue, returning `true` to indicate whether the operation was
@@ -59,22 +54,12 @@ impl<T> BufferedQueue<T> {
     pub fn push(&self, value: T) -> bool {
         match self.ensure_has_space() {
             None => false,
+
             Some(mut queue) => {
                 queue.push_back(value);
-                self.signal_full_queue(queue);
+                self.signal_to_threads(queue, Operation::Push);
                 true
             }
-        }
-    }
-
-    /// checks whether the queue is full and signals to all listening threads, if it wasn't full before
-    fn signal_full_queue(&self, queue: MutexGuard<'_, VecDeque<T>>) {
-        let is_full = queue.len() == self.capacity;
-        let mut existing_state = self.is_full.lock().unwrap();
-
-        if is_full && is_full != *existing_state {
-            *existing_state = true;
-            self.is_full_signal.notify_all();
         }
     }
 
@@ -84,18 +69,40 @@ impl<T> BufferedQueue<T> {
         let mut queue = self.data.lock().unwrap();
         let popped_element = queue.pop_front();
 
-        self.signal_empty_queue(queue);
+        self.signal_to_threads(queue, Operation::Pop);
         popped_element
     }
 
-    /// checks whether the queue is empty and signals to the consumer thread, if it wasn't empty before
-    fn signal_empty_queue(&self, queue: MutexGuard<'_, VecDeque<T>>) {
-        let is_empty = queue.len() == 0;
-        let mut existing_state = self.is_empty.lock().unwrap();
+    fn signal_to_threads(&self, queue: MutexGuard<'_, VecDeque<T>>, operation: Operation) {
+        let order = Ordering::Relaxed;
 
-        if is_empty && is_empty != *existing_state {
-            *existing_state = true;
-            self.is_empty_signal.notify_one();
+        let is_empty = queue.len() == 0;
+        let is_full = queue.len() == self.capacity;
+
+        match operation {
+            //  push => (empty -> false, full -> true?)
+            Operation::Push => {
+                // only update state if queue was previously empty
+                if self.is_empty_.load(order) {
+                    self.is_empty_.store(false, order);
+                };
+
+                if is_full {
+                    self.is_full_.store(true, order);
+                };
+            }
+
+            // pop => (empty -> true?, full -> false)
+            Operation::Pop => {
+                // only update state if queue was previously full
+                if self.is_full_.load(order) {
+                    self.is_full_.store(false, order);
+                };
+
+                if is_empty {
+                    self.is_empty_.store(true, order);
+                }
+            }
         }
     }
 }
